c74f8ab82fb60cd738c62cbb87c487c2071d04a4
Vincent James <viayaqtx>
2021-02-28 15:31:14 +0800

    api

--- src/components/Vote/index.jsx
+++ src/components/Vote/index.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Template from 'components/Template';
 import gradient from 'random-gradient';
 import { v4 as uuidv4 } from 'uuid';
-import { topics } from './Topics';
 import { ImHeart } from 'react-icons/im';
 import { AiFillEye } from 'react-icons/ai';
+import { API } from 'aws-amplify';
 
 export default function Vote() {
+  const [topics, setTopics] = useState([]);
+
+  useEffect(() => {
+    getTopics();
+  }, []);
+
+  const getTopics = async () => {
+    try {
+      const topics = await API.get('topicapi', '/topic');
+      if (topics) {
+        setTopics(topics);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Template>
       <div className="spacer"></div>
@@ -28,9 +45,10 @@ const TopicBox = ({ data }) => {
     padding: '1rem',
     marginBottom: '1rem',
     position: 'relative',
+    cursor: 'pointer'
   };
   return (
-    <div style={styles}>
+    <div style={styles} onClick={}>
       <strong>{data.title}</strong>
       <br />
       <small style={{ color: '#f5f5f5' }}>{data.date}</small>
